a2e0daf1d91d00f5eb9750efef28258fcfb5606d
kurewarvaishnavi05 <kurewarvaishnavi05@gmail.com>
2026-07-06 15:16:01 +0530

    Fix MONGODB_URI error crashing vercel build

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
+  console.warn('WARNING: MONGODB_URI environment variable is missing.');
 }
 
 /**
@@ -22,6 +20,10 @@ if (!cached) {
 async function dbConnect() {
   if (cached.conn) {
     return cached.conn;
+  }
+  
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable in Vercel settings');
   }
 
   if (!cached.promise) {
